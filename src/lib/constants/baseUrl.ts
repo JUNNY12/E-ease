1d99214abe8f831e6fa31ab99727f4bca15f06d5
Juwon <juwonemmanuel22@gmail.com>
2023-10-17 16:26:04 +0100

    change base url

--- src/lib/constants/baseUrl.ts
+++ src/lib/constants/baseUrl.ts
@@ -1,3 +1,1 @@
-export const base_URL = process.env.NODE_ENV === 'development' ?
-process.env.NEXT_PUBLIC_EXTERNAL_API_URL : 
-process.env.NEXT_DEPLOY_EXTERNAL_API_URL;
+export const base_URL = process.env.NEXT_DEPLOY_EXTERNAL_API_URL;
